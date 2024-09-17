4ab228e777afd5f52b2dae18f545513b7c3c9bed
Andre Reder Jureidini <andrereder@gmail.com>
2024-09-17 09:36:38 -0300

    fix - url api pointing to correct api

--- src/pathName.ts
+++ src/pathName.ts
@@ -1,5 +1,5 @@
 export const isLocal = false;
 export const isHomolog = false;
 
-export const pathName = isLocal ? 'http://localhost:3001' : (isHomolog ? 'https://admin-nest-api-d665c1535195.herokuapp.com' : 'https://kiddlepass-admin-api-2e9e4fcd7448.herokuapp.com');
+export const pathName = isLocal ? 'http://localhost:3001' : 'https://constalent-api-2fdefaa0f5d5.herokuapp.com';
 export default pathName;
