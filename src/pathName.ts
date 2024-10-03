8bf826099a97d3a14655fd3dedc39bdf3cd3ad1a
Andre Reder Jureidini <andrereder@gmail.com>
2024-10-03 13:50:57 -0300

    fix - pathpath

--- src/pathName.ts
+++ src/pathName.ts
@@ -1,4 +1,4 @@
-export const isLocal = true;
+export const isLocal = false;
 export const isHomolog = false;
 
 export const pathName = isLocal ? 'http://localhost:3001' : 'https://constalent-api-2fdefaa0f5d5.herokuapp.com';
